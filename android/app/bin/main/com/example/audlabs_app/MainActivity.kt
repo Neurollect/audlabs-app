7a1322e12d2054bd7577d9f07d4d39a1aafa2e5c
JosephGakah <joegakah@gmail.com>
2023-08-21 22:47:19 +0300

    folder structure to the app and file init

---
+++ android/app/bin/main/com/example/audlabs_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.audlabs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/splash.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  SplashState createState() => SplashState();
+}
+
+class SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
